f639600aba1a0a0a00194c5bf1e1c148d64f88f5
kjayanta <kjayanta@thoughtworks.com>
2018-02-11 12:22:24 +0530

    Testing server reads, component not rerendering after state change after mount idk why

--- client/src/chart/PlotlyComponent.js
+++ client/src/chart/PlotlyComponent.js
@@ -1,34 +1,36 @@
-import PropTypes from 'prop-types'
-import Plotly from 'plotly.js'
-const React = require('react');
-// const Plotly = require('plotly.js');
+import PropTypes from "prop-types";
+import Plotly from "plotly.js";
+import React from "react";
 
 class Plot extends React.Component {
-    static propTypes = {
-        handle: PropTypes.string.isRequired,
-        data: PropTypes.array.isRequired,
-        layout: PropTypes.object
-    }
+  static propTypes = {
+    handle: PropTypes.string.isRequired,
+    data: PropTypes.array.isRequired,
+    layout: PropTypes.object
+  };
 
-    componentDidMount() {
-        this.plot(this.props);
-    }
-    componentWillReceiveProps(nextProps) {
-        this.plot(nextProps);
-    }
+  componentDidMount() {
+    console.log('IN COMPONENT DID MOUNT', this.props);
+    this.plot(this.props);
+  }
 
-    plot(props) {
-        let handle = props.handle,
-            data = props.data,
-            layout = props.layout;
-        Plotly.plot(handle, data, layout);
-    }
+  componentWillReceiveProps(nextProps) {
+    console.log('IN COMPONENT WILL RECEIVE PROPS', nextProps);
+    this.plot(nextProps);
+  }
 
-    render() {
-        return (
-            <div id={this.props.handle} >Plot</div>
-        );
-    }
+  plot(props) {
+    let handle = props.handle,
+      data = props.data,
+      layout = props.layout;
+    Plotly.plot(handle, data, layout);
+  }
+
+  render() {
+    return (
+      <div id={this.props.handle}>Plot</div>
+    );
+  }
 }
 
 export default Plot;
--- server/src/app.js
+++ server/src/app.js
@@ -5,12 +5,13 @@ const mongoose = require('mongoose');
 
 app.use(cors());
 mongoose.connect("mongodb://mongo:27017");
-const Cat = mongoose.model('Cat', { name: String });
+const Cat = mongoose.model('Cat', {name: String});
+// const GeoMap = mongoose.model('GeoMap', { name: String, data: });
 
 // const kitty = new Cat({ name: 'Zildjian' });
 // kitty.save().then(() => console.log('meow'));
 
-app.get('/openingMessage', function(req, res){
+app.get('/openingMessage', function (req, res) {
   let cats = Cat.find(function (err, cats) {
     if (err) return console.error(err);
     console.log(cats);
@@ -18,6 +19,52 @@ app.get('/openingMessage', function(req, res){
   });
 });
 
-app.listen(3001, function(){
+app.get('/map/data', function (req, res) {
+  res.send({
+    data: [
+      {
+        lat: [12.976135, 13.976135],
+        lon: [77.592775, 78.592775],
+        text: ['First', 'Second'],
+        type: 'scattergeo',
+        mode: 'markers+text',
+        name: 'Points'
+      }
+    ],
+    layout: {
+      title: 'Birds of the Western Ghats',
+      autosize: true,
+      width: 1000,
+      height: 800,
+      margin: {
+        l: 65,
+        r: 50,
+        b: 65,
+        t: 90
+      },
+      geo: {
+        scope: 'india',
+        resolution: 50,
+        lataxis: {
+          'range': [0, 30]
+        },
+        lonaxis: {
+          'range': [50, 100]
+        },
+        showrivers: true,
+        rivercolor: '#fff',
+        showlakes: true,
+        lakecolor: '#fff',
+        showland: true,
+        landcolor: '#EAEAAE',
+        countrycolor: '#d3d3d3',
+        countrywidth: 1.5,
+        subunitcolor: '#d3d3d3'
+      }
+    }
+  })
+});
+
+app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
 });
