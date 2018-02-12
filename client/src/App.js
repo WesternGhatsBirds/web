46ed4abf0aa02ee977e5c5dfe5777370ecdec825
kjayanta <kjayanta@thoughtworks.com>
2018-02-12 14:41:39 +0530

    Introduce bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import "./App.css";
 import BirdsMap from "./map/BirdsMap";
 import BirdsMapG from "./map/BirdsMapG";
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
   constructor(props) {
@@ -17,8 +18,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">Birds Of the Western Ghats</NavbarBrand>
+        </Navbar>
         <BirdsMapG source={'/map/data'}/>
-        <BirdsMap source={'/map/data'}/>
+        {/*<BirdsMap source={'/map/data'}/>*/}
       </div>
     );
   }
